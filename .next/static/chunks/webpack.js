de76929a702fae8521192d8338cf7d97682ee332
andrewscarlos <andrews.knct@gmail.com>
2022-03-27 15:08:57 -0300

    feat: add stripe api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dfe956b2d0e6e3de"; }
+/******/ 		__webpack_require__.h = function() { return "0aa31b29da2549d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig News</title>
-        
+        <title>Home | Ig News</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span> Hey, welecome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $9.90 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl conding" />
+      </main>
     </>
   );
 }
