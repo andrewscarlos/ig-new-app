59d856d6d63963339ebae2bf4f237ae9359ade93
andrewscarlos <andrews.knct@gmail.com>
2022-03-27 18:35:10 -0300

    feat: add fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,22 @@
+import { fauna } from "../../../services/fauna";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query } from "faunadb";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      await fauna.query(
+        query.Create(query.Collection("users"), { date: { email } })
+      );
+      return true;
+    },
+  },
 });
